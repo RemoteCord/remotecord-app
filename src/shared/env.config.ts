1dacdf69bfbde4eb6bd1388d5f190912124eb9c5
luqueee <luqueee_@outlook.es>
2025-04-22 23:06:52 +0200

    fix: remove trailing comma from VITE_AUTH0_AUDIENCE in environment configuration

--- src/shared/env.config.ts
+++ src/shared/env.config.ts
@@ -3,5 +3,5 @@ export const env = {
 	VITE_WS_URL: import.meta.env.VITE_WS_URL,
 	VITE_AUTH0_CLIENT_ID: import.meta.env.VITE_AUTH0_CLIENT_ID,
 	VITE_AUTH0_DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN,
-	VITE_AUTH0_AUDIENCE: import.meta.env.VITE_AUTH0_AUDIENCE,
+	VITE_AUTH0_AUDIENCE: import.meta.env.VITE_AUTH0_AUDIENCE
 };
