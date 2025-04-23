892ef99303d13288daa950a76ab26e8c3f416e60
luqueee <luqueee_@outlook.es>
2025-04-23 21:36:55 +0200

    refactor: streamline request handling in useApi hook and improve error logging

--- src/hooks/common/useApi.tsx
+++ src/hooks/common/useApi.tsx
@@ -12,32 +12,47 @@ export const useApi = () => {
     input: URL | Request | string,
     init?: RequestInit & ClientOptions
   ) => {
-    // console.log("REQUEST TO", `${env.VITE_API_URL}${input}`);
-    // if (!user) return;
-    // if (!isAuthenticated) {
-    //   navigator("/auth");
-    //   return;
-    // }
-    const token = await authStore.getRecord<string>("token");
-    // if (!token) navigator("/auth");
-    // const { session } = (await supabase?.auth.getSession())?.data || {};
-    // const { token } = auth;
-    console.log("REQUEST TO", `${env.VITE_API_URL}${input}`, init, token);
-    const headersData = {
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${token}`,
-        ...init?.headers,
-      },
-      ...init,
-    };
+    try {
+      // console.log("REQUEST TO", `${env.VITE_API_URL}${input}`);
+      // if (!user) return;
+      // if (!isAuthenticated) {
+      //   navigator("/auth");
+      //   return;
+      // }
+      const token = await authStore.getRecord<string>("token");
+      // if (!token) navigator("/auth");
+      // const { session } = (await supabase?.auth.getSession())?.data || {};
+      // const { token } = auth;
+      console.log("REQUEST TO", `${env.VITE_API_URL}${input}`, init, token);
+      const headersData = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+          ...init?.headers,
+        },
+        ...init,
+      };
 
-    const response = await fetch(`${env.VITE_API_URL}${input}`, headersData);
-    const res = await response.json();
+      const response = await fetch(`${env.VITE_API_URL}${input}`, headersData);
 
-    console.log("RESPONSE", res);
+      console.log("RESPONSE", response);
 
-    return res as T;
+      const res = await response.json();
+
+      console.log("RESPONSE DATA", res);
+
+      return res as T;
+    } catch (error) {
+      console.log("ERROR", error);
+      if (error instanceof Error) {
+        console.log("ERROR MESSAGE", error.message);
+      }
+      if (error instanceof Response) {
+        const res = await error.json();
+        console.log("RESPONSE ERROR", res);
+      }
+    }
   };
+
   return { request };
 };
