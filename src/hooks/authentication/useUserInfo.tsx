8a1f2dc6b20c44b12a89e672ca6a3eaa1d81bd00
luqueee <luqueee_@outlook.es>
2025-04-23 21:44:50 +0200

    refactor: standardize formatting and improve readability in useUserInfo hook

--- src/hooks/authentication/useUserInfo.tsx
+++ src/hooks/authentication/useUserInfo.tsx
@@ -4,27 +4,27 @@ import { useDebouncedSync } from "@/hooks/common/useDebounceSync";
 import { useEffect, useState } from "react";
 
 export const useUserInfo = () => {
-	const { request } = useApi();
-	const [username, setUsername] = useState<string>("");
-	useEffect(() => {
-		request<{ username: string }>("/api/clients/user-info").then((res) => {
-			console.log("res", res);
-			if (res) setUsername(res.username);
-		});
-	}, []);
+  const { request } = useApi();
+  const [username, setUsername] = useState<string>("");
+  useEffect(() => {
+    request<{ username: string }>("/api/clients/user-info").then((res) => {
+      console.log("res", res);
+      if (res) setUsername(res.username);
+    });
+  }, []);
 
-	const handleChangeUsername = () => {
-		console.log("newUsername", username);
-		request<{ status: boolean }>("/api/clients/user-name", {
-			method: "POST",
-			body: JSON.stringify({ username: username }),
-		}).then((res) => {
-			console.log("res", res);
+  const handleChangeUsername = () => {
+    console.log("newUsername", username);
+    request<{ status: boolean }>("/api/clients/user-name", {
+      method: "POST",
+      body: JSON.stringify({ username: username }),
+    }).then((res) => {
+      console.log("res", res);
 
-			res.status && setUsername(username);
-		});
-	};
+      res?.status && setUsername(username);
+    });
+  };
 
-	useDebouncedSync(username, handleChangeUsername, 500);
-	return { username, handleChangeUsername, setUsername };
+  useDebouncedSync(username, handleChangeUsername, 500);
+  return { username, handleChangeUsername, setUsername };
 };
